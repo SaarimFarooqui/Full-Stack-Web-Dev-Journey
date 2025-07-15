3353ef8e735b1c22374f46d1a2471ebdf212c44d
Saarim Farooqui <saarimfarooqui000@gmail.com>
2025-07-15 23:07:32 +0500

    Updated optimized bubble sort using swap functionality

--- Javascript/bubblesort.js
+++ Javascript/bubblesort.js
@@ -1,15 +1,17 @@
-let list = [1,5,7,3,2,4,9,8,6,1,4,6,3,7,9,10,11,10,4,2,4,5,7,12,13,0,11]
-
 let bubbleSort = (list) => {
-    for(let j = 0;  j < list.length; j++){
-        for (let i = 0; i < list.length-1; i++){
-        if (list[i] > list[i+1]){
-            [list[i], list[i+1]] = [list[i+1], list[i]]
-            }
-        }
-    }
-}
+  let n = list.length;
+  let swapped;
 
-console.log(list);
-bubbleSort(list);
-console.log(list);
+  do {
+    swapped = false;
+
+    for (let i = 0; i < n - 1; i++) {
+      if (list[i] > list[i + 1]) {
+        [list[i], list[i + 1]] = [list[i + 1], list[i]];
+        swapped = true;
+      }
+    }
+
+    n--; 
+  } while (swapped);
+};
