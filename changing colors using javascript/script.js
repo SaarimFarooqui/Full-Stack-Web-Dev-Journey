2b2272e8ee84ab2e548c03baebe8efdbcafdc5ad
Saarim Farooqui <saarimfarooqui000@gmail.com>
2025-07-17 14:03:03 +0500

    I added multiple colors functionality

--- changing colors using javascript/script.js
+++ changing colors using javascript/script.js
@@ -2,20 +2,32 @@ let leftbox = document.getElementById("left");
 let rightbox = document.getElementById("right");
 
 const gradients = [
-  "linear-gradient(45deg, #ff6a00, #ee0979)",
-  "linear-gradient(45deg, #00c6ff, #0072ff)",
-  "linear-gradient(45deg, #f7971e, #ffd200)",
-  "linear-gradient(45deg, #43cea2, #185a9d)",
-  "linear-gradient(45deg, #7b4397, #dc2430)",
-  "linear-gradient(45deg, #00b09b, #96c93d)",
-  "linear-gradient(45deg, #f953c6, #b91d73)",
-  "linear-gradient(45deg, #3a1c71, #d76d77, #ffaf7b)",
-  "linear-gradient(45deg, #1f4037, #99f2c8)",
-  "linear-gradient(45deg, #0f2027, #203a43, #2c5364)"
+  "linear-gradient(45deg, #c800ffff, #ee0979)",
+  "linear-gradient(45deg, #ff0000ff, #1900ffff)",
+  "linear-gradient(45deg, #ff9500ff, #11ff00ff)",
+  "linear-gradient(45deg, #00ffaeff, #76baffff)",
 ];
+let sign = false
 
 setInterval(()=>{
- let currentopacity = parseFloat(getComputedStyle(rightbox).opacity);
- currentopacity -= 0.01;
- rightbox.style.opacity = currentopacity;
+ let rightcurrentopacity = parseFloat(getComputedStyle(rightbox).opacity);
+ if (rightcurrentopacity == 0){ 
+  let randomInd = Math.floor(Math.random() * 3);
+  rightbox.style.background = gradients[randomInd];
+  sign = true;  
+}
+ else if (rightcurrentopacity == 1){ 
+  let randomInd = Math.floor(Math.random() * 3);
+  leftbox.style.background = gradients[randomInd];
+  sign = false
+}
+
+ if(sign == false){
+  rightcurrentopacity -= 0.01;
+  rightbox.style.opacity = rightcurrentopacity;
+ }else if(sign == true){
+  rightcurrentopacity += 0.01;
+  rightbox.style.opacity = rightcurrentopacity;
+ }
+
 }, 10)
