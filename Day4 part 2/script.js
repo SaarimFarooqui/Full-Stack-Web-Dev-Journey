af817a9471574f806ebe2f18b3cfffdebe69e8eb
Saarim Farooqui <saarimfarooqui000@gmail.com>
2025-07-25 00:12:48 +0500

    Completed the part 2 of day 4

--- Day4 part 2/script.js
+++ Day4 part 2/script.js
@@ -1,9 +1,23 @@
-let headBar = document.getElementById("heading");
+let container = document.getElementById("container");
+let btnAdd = document.getElementById("add");
+let btnRemover = document.getElementById("remove");
 
-setInterval(() => {
-    if (rgb(0, 0, 0) == getComputedStyle(headBar).backgroundColor) {
-        headBar.style.backgroundColor = "rgb(0, 2, 99)";
-    } else {
-        headBar.style.backgroundColor = "black";
+btnAdd.addEventListener("click", () => {
+    let newdiv = document.createElement("div");
+    newdiv.style.backgroundColor = "black";
+    newdiv.style.width = "100%";
+    newdiv.style.height = "1in";
+    newdiv.style.transition = "all 0.5s ease"
+    newdiv.style.opacity = "0";
+    setTimeout(() => {
+        newdiv.style.opacity = "1";
+    },10)
+    container.appendChild(newdiv);
+});
+
+btnRemover.addEventListener("click", () => {
+    const lastdiv = container.lastElementChild;
+    if (lastdiv){
+    container.removeChild(lastdiv);
     }
-}, 1000);
+})
