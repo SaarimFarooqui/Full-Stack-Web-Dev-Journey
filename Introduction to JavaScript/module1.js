6d4cdb6d3be646d256a26d1e5d3bd6403e07ae2f
Saarim Farooqui <saarimfarooqui000@gmail.com>
2025-09-27 14:39:04 +0500

    JavaScript Practice

--- Introduction to JavaScript/module1.js
+++ Introduction to JavaScript/module1.js
@@ -1,3 +1,37 @@
-let  a = 2;
-let b = "2";
-console.log((a == b)?console.log("Condition fulfilled"):console.log("False Condition"));
+/* 
+list = ["Apple","Guava","Melon","Cherry","Blueberry","Custard Apple","Grape"]
+
+list.forEach(name => {
+    console.log(name);
+});
+list.forEach((name,index) => {
+    console.log(`${index+1} ${name}`);
+}); 
+
+for(a of list){
+    console.log(a);
+}
+for(a in list){
+    console.log(a);
+}
+    
+let a = 10;
+if(a==10? console.log("Its 10"):console.log("Its not 10"));
+
+const sur_name = Symbol("Farooqui");
+const user = {
+    name: "Saarim",
+    [sur_name]: "Farooqui"
+};
+console.log(user[sur_name]);
+console.log(Object.keys(user));
+console.log(user.name + " " + user.sur_name)
+*/
+let id = Symbol("fulana")
+
+person = {
+    name : "Ahmed Saarim Uddin Farooqui",
+    [id] : "22sp-004-cs"
+}
+
+console.log(person[id]);
