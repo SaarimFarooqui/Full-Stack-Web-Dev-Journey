a23b8345d742a6f39c108191e513e8b2b743abcd
Saarim Farooqui <saarimfarooqui000@gmail.com>
2025-09-30 13:04:58 +0500

    Irritating x scrollbar issue resolved

---
+++ Using ForEach in Web Page/script.js
@@ -0,0 +1,8 @@
+let box = document.getElementById("background");
+let addbtn = document.getElementById("add");
+let subbtn = document.getElementById("sub");
+
+addbtn.addEventListener("click", ()=>{
+    let newdiv = document.createElement("div");
+    newdiv.classList.add("")
+})
