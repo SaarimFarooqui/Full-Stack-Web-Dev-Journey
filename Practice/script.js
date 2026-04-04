20ed286fc0dd8d851290a7e5b5febe4f8aa45935
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-04-04 17:01:54 +0500

    Solved the padding outside the parent problem

--- Practice/script.js
+++
@@ -1,7 +0,0 @@
-async function printData(){
-  const api = await fetch('https://jsonplaceholder.typicode.com/todos');
-  const data = await api.json();
-  data.forEach((obj) => {console.log(obj)})
-}
-
-printData()
