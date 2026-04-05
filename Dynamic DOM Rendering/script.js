daf33921c765fcee3e66bc599caa9b9543719ebf
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-04-05 12:08:39 +0500

    Data for food items created

---
+++ Dynamic DOM Rendering/script.js
@@ -0,0 +1,26 @@
+data = [
+    {
+        name: "Biryani",
+        price: 200
+    },
+    {
+        name: "Chicken Qorma",
+        price: 220
+    },
+    {
+        name: "Chicken Karahi",
+        price: 250
+    },
+    {
+        name: "Lab e Shireen",
+        price: 400
+    },
+    {
+        name: "Daal Chana",
+        price: 150
+    },
+    {
+        name: "Lassi",
+        price: 150
+    }
+]
