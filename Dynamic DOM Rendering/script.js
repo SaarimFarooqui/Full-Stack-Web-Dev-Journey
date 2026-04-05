f9fb6feea583573da4fa321d17289f97008d19dd
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-04-05 14:41:05 +0500

    Sucessfully arranged six cards with data from javascript list of objects in it.

--- Dynamic DOM Rendering/script.js
+++ Dynamic DOM Rendering/script.js
@@ -1,26 +1,28 @@
-data = [
-    {
-        name: "Biryani",
-        price: 200
-    },
-    {
-        name: "Chicken Qorma",
-        price: 220
-    },
-    {
-        name: "Chicken Karahi",
-        price: 250
-    },
-    {
-        name: "Lab e Shireen",
-        price: 400
-    },
-    {
-        name: "Daal Chana",
-        price: 150
-    },
-    {
-        name: "Lassi",
-        price: 150
-    }
+const data = [
+    { name: "Biryani", price: 200 },
+    { name: "Chicken Qorma", price: 220 },
+    { name: "Chicken Karahi", price: 250 },
+    { name: "Lab e Shireen", price: 400 },
+    { name: "Daal Chana", price: 150 },
+    { name: "Lassi", price: 150 }
 ]
+
+const app = document.getElementById("app");
+let html = "";
+data.forEach(element => {
+    html += `
+        <div class="card">
+            <div class="card-portion">
+                <p class="productName">
+                    ${element.name}
+                </p>
+            </div>
+            <div class="card-portion">
+                <p class="price">
+                    PKR ${element.price}
+                </p>
+            </div>
+        </div>`
+});
+
+app.innerHTML = html;
