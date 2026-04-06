a014ec8f87ec75bf3c442426c3c11443dcd72098
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-04-06 06:42:51 +0500

    Dynamic DOM rendering using for loop iteration of a fixed number

--- Dynamic DOM Rendering/script.js
+++ Dynamic DOM Rendering/script.js
@@ -1,28 +1,10 @@
-const data = [
-    { name: "Biryani", price: 200 },
-    { name: "Chicken Qorma", price: 220 },
-    { name: "Chicken Karahi", price: 250 },
-    { name: "Lab e Shireen", price: 400 },
-    { name: "Daal Chana", price: 150 },
-    { name: "Lassi", price: 150 }
-]
+const boxes = 10;
 
-const app = document.getElementById("app");
-let html = "";
-data.forEach(element => {
-    html += `
-        <div class="card">
-            <div class="card-portion">
-                <p class="productName">
-                    ${element.name}
-                </p>
-            </div>
-            <div class="card-portion">
-                <p class="price">
-                    PKR ${element.price}
-                </p>
-            </div>
-        </div>`
-});
+let html ="";
 
-app.innerHTML = html;
+for (let i = 1;  i <= boxes; i++){
+    html += `<div class="box"></div>`
+}
+const main = document.getElementById("main");
+
+main.innerHTML = html;
