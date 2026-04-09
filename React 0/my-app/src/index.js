57365f2f5f4668a870a78db1aa79cd8483ff0384
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-04-09 14:15:47 +0500

    Created a beautiful Navbar

--- React 0/my-app/src/index.js
+++ React 0/my-app/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 
 const App = () => { 
-  return <div><LandingPage/></div>
+  return <div><FirstScreen/></div>
 }
-const LandingPage = () => {
-  return <div className='LandingPage'></div>
+const FirstScreen = () => {
+  return <div className='Background'>
+    <div className='Navbar'>
+      <ul className='UnorderedList'>
+        <li className='List'><a href='www.google.com'>Home</a></li>
+        <li className='List'><a>About</a></li>
+        <li className='List'><a>Contact</a></li>
+      </ul>
+    </div>
+  </div>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
