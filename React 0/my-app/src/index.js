de514924b055d30ef93305a17cbb2873db990333
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-04-09 10:49:48 +0500

    In react, changed the landing page background to conic gradient

--- React 0/my-app/src/index.js
+++ React 0/my-app/src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 
 const App = () => { 
-  return <div>
-    <h1>What is Lorem Ipsum</h1>
-    <Header/>
-  </div>
+  return <div><LandingPage/></div>
 }
-const Header = () => {
-  return <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+const LandingPage = () => {
+  return <div className='LandingPage'></div>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
