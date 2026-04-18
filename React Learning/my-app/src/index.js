0fd0752131044db13e8510a58ab22cd2a3aed722
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-04-18 16:35:16 +0500

    Learned how to use conditional classes by using css variant class

--- React Learning/my-app/src/index.js
+++ React Learning/my-app/src/index.js
@@ -47,36 +47,14 @@ const pizzaData = [
     soldOut: false,
   },
 ];
-
+const check = true;
 const App = () => { 
   return <div><Background/></div>
 }
 
 const Background = () =>{
-  return<div id="Background">
-    {
-      pizzaData.map((pizza) => {
-        return <Card obj = {pizza} />
-      })
-    }
-  </div>
+  return<div className={`Background ${check ? "Background--new" : ""}`}></div>
 }
 
-const Card = (props) => {
-  return <div id = "Card">
-    <div className='Portion' id='CardLeft'>
-      {
-      props.obj.soldOut ? <img id='NotAvailable' src={props.obj.photoName} alt='PizzaImage'></img> : 
-      <img src={props.obj.photoName} alt='PizzaImage'></img>
-      }
-    </div>
-    <div className='Portion' id='CardRight'>
-      <p className = "CardText" id="PizzaName">{props.obj.name}</p>
-      <p className = "CardText" id="Ingredients">{props.obj.ingredients}</p>
-      <p className = "CardText" id="Price">USD {props.obj.price}</p>
-      {props.obj.soldOut && <p id="OutOfStock">Not Available</p>}
-    </div>
-  </div>
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode><App/></React.StrictMode>)
