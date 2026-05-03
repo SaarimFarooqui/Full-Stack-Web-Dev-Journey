ddf91c366e882b55ccbccde7bcac6e519e516a74
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-05-03 04:03:05 -0700

    Refreshing the concept of destructuring in javascript

---
+++ Practice/script.js
@@ -0,0 +1,20 @@
+const students = [
+    {
+    name : "Saarim",
+    roll : "22sp-004-cs",
+    age : 23
+    },
+    {
+    name : "Faraz",
+    roll : "22sp-017-cs",
+    age : 23
+    },
+    {
+        name : "Hassan",
+        roll : "22sp-018-cs",
+    age : 23
+    }
+]
+
+const [student1,student2] = students;
+console.log(student1)
