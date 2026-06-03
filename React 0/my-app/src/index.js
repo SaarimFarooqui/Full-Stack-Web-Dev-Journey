91998befb8c323845c3e040657f6ea823ab05acf
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-06-03 04:49:28 -0700

    Successfully created a counter using state

--- React 0/my-app/src/index.js
+++ React 0/my-app/src/index.js
@@ -5,33 +5,22 @@ import './animations.css'
 
 
 const App = () => { 
-  return <><Background/></>
-}
-const Background = () =>{
-  return<div id="background"><Board/></div>
-}
 
-const Board = () => {
-  
-  const textmessage = [
-    "Knowledge without action is wastefulness, and action without knowledge is foolishness.",
-    "Let yourself be silently drawn by the strange pull of what you really love. It will not lead you astray.",
-    "The world is divided into men who have wit and no religion and men who have religion and no wit."
-  ]
-  const [num, setNum] =  useState(0);  
-  
-  return <div id='board'>
-    <ul id='numbering'>
-      <li className={num >= 0 ? 'active' : ''}>1</li>
-      <li className={num >= 1 ? 'active' : ''}>2</li>
-      <li className={num >= 2 ? 'active' : ''}>3</li>
-    </ul>
-    <p id='text'>{textmessage[num]}</p>
-    <div id='buttons'>
-      <button onClick={() => num > 0 && setNum(num-1)}>Prev</button>
-      <button onClick={() => num < 2 && setNum(num+1)}>Next</button>
+  const [count, setCount] = useState(0);
+
+  return <>
+  <div id="background">
+    <div id='box'>
+      <div id='upper'>{count}</div>
+      <div id='lower'>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </div>
     </div>
   </div>
+  </>
 }
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode><App/></React.StrictMode>)
