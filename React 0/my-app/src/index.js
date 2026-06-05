73c9397b53f40551ae9dccc1173b6251b9be84ad
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-06-05 11:53:32 +0500

    For a date counter, successfully created a statement with nested circuit and ternaries

--- React 0/my-app/src/index.js
+++ React 0/my-app/src/index.js
@@ -6,19 +6,28 @@ import './animations.css'
 
 const App = () => { 
 
+  const [dateCount, setDateCount] = useState(0);
+
   const date = new Date();
-  const formatted = date.toLocaleTimeString('en-US', {
+  date.setDate(date.getDate() + dateCount)
+
+  const formatted = date.toLocaleDateString('en-US', {
     weekday: "long",
     day: "numeric",
     month: "long",
     year: "numeric"
   })
-  
-  const [count, setCount] = useState(0);
+  const dds = dateCount ** 2 === 1 ? "day" : "days"
+  const statement = ((dateCount === 0) && `Today is ${formatted}`) || ((dateCount < 0) ? `${dateCount * -1} ${dds} ago was ${formatted}` : `${dateCount} ${dds} from now will be ${formatted}`)  
+
 
   return <>
   <div id="background">
-    <div id='box'></div>
+    <div id='box'>
+    <div id='first'></div>
+    <div id='second'></div>
+    <div id='third'>{statement}</div>
+    </div>
   </div>
   </>
 }
