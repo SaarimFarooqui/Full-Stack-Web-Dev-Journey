3453a55c06454247769a1cad23c7089813b133f5
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-06-11 16:33:51 +0500

    Started learning forms

--- React 0/my-app/src/index.js
+++ React 0/my-app/src/index.js
@@ -5,33 +5,8 @@ import './animations.css'
 
 
 const App = () => { 
-
-  const [dateCount, setDateCount] = useState(0);
-
-  const date = new Date();
-  date.setDate(date.getDate() + dateCount)
-
-  const formatted = date.toLocaleDateString('en-US', {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric"
-  })
-  const dds = dateCount ** 2 === 1 ? "day" : "days"
-  const statement = ((dateCount === 0) && `Today is ${formatted}`) || ((dateCount < 0) ? `${dateCount * -1} ${dds} ago was ${formatted}` : `${dateCount} ${dds} from now will be ${formatted}`)  
-
-
-  return <>
-  <div id="background">
-    <div id='box'>
-    <div id='first'></div>
-    <div id='second'></div>
-    <div id='third'>{statement}</div>
-    </div>
-  </div>
-  </>
+  return <></>
 }
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode><App/></React.StrictMode>)
