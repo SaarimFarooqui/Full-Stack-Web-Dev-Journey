cea54ee23a6fc6fca99bf74f4a33476e1a14736d
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-06-12 07:57:47 +0500

    Just started creating top adding items bar

--- React 0/my-app/src/index.js
+++ React 0/my-app/src/index.js
@@ -5,7 +5,13 @@ import './animations.css'
 
 
 const App = () => { 
-  return <></>
+  return <> 
+  <div id='background'>
+    <div id='top-bar'>
+      <div id='add-your-items'></div>
+    </div>
+  </div> 
+  </>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
