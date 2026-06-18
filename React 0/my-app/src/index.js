9e7f65939a68b1001af023dd0738dff88ba7e177
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-06-18 10:43:05 +0500

    Successfully used input and select tag values to put information on the screen

--- React 0/my-app/src/index.js
+++ React 0/my-app/src/index.js
@@ -6,8 +6,19 @@ import './animations.css'
 
 const App = () => { 
 
-  const submitter = () => {
-    
+  const [quantity, setQuantity] = useState(1);
+  const [product, setProduct] = useState("");
+  const [items, setItems] = useState([]);
+  
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const newitem = {
+      number_of_items : quantity,
+      item_name : product
+    }
+    setItems((prev) => [...prev, newitem]);
+    setProduct("");
+    setQuantity(1);
   }
 
   return <> 
@@ -18,11 +29,16 @@ const App = () => {
     <div id='form-bar'>
       <form>
         <empty></empty>
-        <select>{Array.from({length:10}, (_ , i)=> i + 1).map((i)=> <option value={i} key={i}>{i}</option>)}</select>
-        <input></input>
-        <button>Submit</button>
+        <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+          {Array.from({length:10}, (_ , i)=> (i - 1) + 1).map((i)=> <option value={i} key={i}>{i}</option>)}
+        </select>
+        <input value={product} onChange={(e) => setProduct(e.target.value)}></input>
+        <button onClick={handleAdd}>Add</button>
         <empty></empty>
       </form>
+    </div>
+    <div id='showlist'>
+      {items.map((item) => <p>The number of {item.item_name} we have is {item.number_of_items}</p>)}
     </div>
   </div>
   </>
