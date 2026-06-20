3cfe0acd32461596798dc3bf0b4d3fa97070e42c
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-06-20 16:33:59 +0500

    Amazing animation effect added

--- Text Animation Effect/script.js
+++ Text Animation Effect/script.js
@@ -1,3 +1,4 @@
 let text = document.querySelector(".text");
-let spans = text.innerText.split('');
+let spans = text.innerText.split('').map((letter,i)=> `<span>${letter}</span>`).join('');
+text.innerHTML = spans;
 console.log(spans);
