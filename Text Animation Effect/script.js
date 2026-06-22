ccb22d23c88fb174bd9aafdad592b95e84fef7ce
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-06-22 12:39:48 +0500

    Successfully completed the text animation

--- Text Animation Effect/script.js
+++ Text Animation Effect/script.js
@@ -1,4 +1,4 @@
 let text = document.querySelector(".text");
-let spans = text.innerText.split('').map((letter,i)=> `<span>${letter}</span>`).join('');
+let spans = text.innerText.split('').map((letter,i)=> `<span style = "transition-delay: ${i*40}ms; filter: hue-rotate(${i*20}deg)" >${letter}</span>`).join('');
 text.innerHTML = spans;
 console.log(spans);
